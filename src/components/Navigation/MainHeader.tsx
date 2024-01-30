0c4b98c4c423c099b1ee1ffb442d341dcaf9a2c2
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-01-30 10:04:43 -0600

    Updated, MainHeader and  Root and Home components

--- src/components/Navigation/MainHeader.tsx
+++ src/components/Navigation/MainHeader.tsx
@@ -1,6 +1,31 @@
+//import { NavLink } from 'react-router-dom'
+import Button from '../UI/Button'
+
 export default function MainHeader() {
     return (
         <>
+            <header id='main-header'>
+                <h1>ReactMentoring</h1>
+                <nav>
+                    <ul>
+                        {/* <li>
+                            <NavLink to='/'>Our Mission</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/sessions'>Browse Sessions</NavLink>
+                        </li> */}
+                        <li>
+                            <a>Our Mission</a>
+                        </li>
+                        <li>
+                            <a>Browse Sessions</a>
+                        </li>
+                        <li>
+                            <Button>Upcoming Sessions</Button>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         </>
     )
 }
